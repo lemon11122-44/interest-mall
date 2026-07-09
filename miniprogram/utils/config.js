80ed67aebcae69cc77f6d5784e966e0a853e9ca4
user <user@example.com>
2026-07-10 00:22:37 +0800

    update: set WeChat Cloud Hosting domain in config

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -3,7 +3,7 @@
 // 生产环境：改成微信云托管生成的域名
 const CONFIG = {
   // API_BASE: "http://127.0.0.1:8000",           // 本地开发
-  API_BASE: "https://你的云托管域名", // ⬅️ 部署微信云托管后替换
+  API_BASE: "https://lemon1-280065-9-1452195104.sh.run.tcloudbase.com", // 微信云托管
 };
 
 module.exports = CONFIG;
